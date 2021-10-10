0a6dd8bdf0023386f0b143179024741ebfd70c34
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-10-11 02:42:45 +0700

    Object 2 done

---
+++ 02-object2.js
@@ -0,0 +1,31 @@
+//NO 2 DONE 
+function getObjectValue(obj, path) { 
+  let newPath = path.split('.')
+  let result = obj[newPath[0]][newPath[1]][newPath[2]]
+  if(result === undefined) return null
+  else return result
+}
+
+const milkBasedCoffee = {
+  name: "latte",
+  ingredients: {
+    espresso: {
+      origin: "lampung",
+      roastProfile: "medium to dark",
+      ratio: 1
+    },
+    milk: {
+      brand: "susu murni",
+      isVegan: false,
+      ratio: 5
+    }
+  },
+}
+
+const espresso = getObjectValue(milkBasedCoffee, "ingredients.espresso.origin");
+const coffeeBrand = getObjectValue(milkBasedCoffee, "ingredients.espresso.brand");
+const isMilkVegan = getObjectValue(milkBasedCoffee, "ingredients.milk.isVegan");
+
+console.log(espresso);
+console.log(coffeeBrand);
+console.log(isMilkVegan)
