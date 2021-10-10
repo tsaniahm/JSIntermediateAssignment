0b2dacfa5eed18c0eaaaec54a288b5eb7a123c39
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-10-11 02:31:17 +0700

    Array 1 Done

--- 01-array.js
+++ 01-array.js
@@ -1,64 +1,17 @@
-//NO 1
-// const arr = [1, 2, 3];
-// const arr2 = [8, 1, 5, 7];
+//NO 1 DONE
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
 
-// function reverse(array) {
-//     return array.reverse()
-// }
-
-// const newArr = reverse(arr);
-// const newArr2 = reverse(arr2);
-
-// console.log(arr, newArr);
-// console.log(arr2, newArr2);
-
-//NO 2 DONE
-// const arr1 = [1, 3, 4, 1, 2, 8];
-// const arr2 = [5, 6, 7, 8, 1, 3];
-
-// function getAverage( array) {
-//     let total = 0;
-//     let total2 = 0;
-    
-//     for(let i =0 ; i < array.length ;i++){
-//         total = total + array[i]
-//     }
-
-//     let avg = total / array.length;
-
-//     for(let i =0 ; i < array.length ;i++){
-//         if(array[i] > avg){
-//             total2 = total2 + 1
-//         }
-//     }
-//     return total2
-// }
-
-// console.log(getAverage(arr1))
-// console.log(getAverage(arr2))
-
-//NO 3 DONE TAPI YG GA ADA BUKANNYA NULL MALAH UNDEFINED
-const arr = [
-    [10],
-    [9, 7, 1],
-    [2, 8],
-  ];
-  
-  function searchInArray(array, number) {
-    let newArr = []
-    for(let i = 0; i < array.length; i++)
-    {
-        newArr = newArr.concat(array[i]);
+function reverse(array) {
+    let result = new Array;
+    for(let i = array.length-1; i >= 0; i--) {
+        result.push(array[i])
     }
+    return result;
+}
 
-    for(let i=0; i<newArr.length; i++){
-        if(newArr[i] == number){
-            return i 
-        }
-    }
-   }
-  
-  console.log(searchInArray(arr, 3));
-  console.log(searchInArray(arr, 2));
-  console.log(searchInArray(arr, 4));
-  console.log(searchInArray(arr, 8));
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
+
+console.log(arr, newArr);
+console.log(arr2, newArr2);
---
+++ 01-array2.js
@@ -0,0 +1,24 @@
+//NO 2 DONE
+const arr1 = [1, 3, 4, 1, 2, 8];
+const arr2 = [5, 6, 7, 8, 1, 3];
+
+function getAverage( array) {
+    let total = 0;
+    let count = 0;
+    
+    for(let i =0 ; i < array.length ;i++){
+        total = total + array[i]
+    }
+
+    let avg = total / array.length;
+
+    for(let i =0 ; i < array.length ;i++){
+        if(array[i] > avg){
+            count = count + 1
+        }
+    }
+    return count
+}
+
+console.log(getAverage(arr1))
+console.log(getAverage(arr2))
